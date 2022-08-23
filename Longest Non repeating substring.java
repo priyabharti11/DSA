5e115034bce0f6b4b7f7eb4169b01f4a5c945332
Priya Bharti <91482888+priyabharti11@users.noreply.github.com>
2022-08-23 22:47:45 +0530

    Medium (HashMap)

---
+++ Longest Non repeating substring.java
@@ -0,0 +1,17 @@
+//Returning maximum substring's length from a given string
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       if(s.length()==0) return 0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        int max=0;
+        //2 pointers
+        for(int i=0,j=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                j=Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max=Math.max(max,i-j+1);
+        }
+        return max;
+    }
+}
